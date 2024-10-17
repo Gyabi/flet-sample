9563874e33cdf12a164914534c98149bbfc3f296
Gyabi <buyuuka12345@gmail.com>
2024-10-17 21:50:58 +0900

    追記

--- main.py
+++ main.py
@@ -13,17 +13,94 @@ async def main(page: ft.Page):
     
     
         
-    view = navigation_view(
-        page=page,
-        contents=[
-            NavigationContent(title="Item 1", icon=ft.icons.ADD_TO_HOME_SCREEN_SHARP, view=map()),
-            NavigationContent(title="Item 2", icon=ft.icons.ADD_COMMENT, view=ft.Text("Item 2")),
-            NavigationContent(title="Item 3", icon=ft.icons.ADD_COMMENT, view=SampleControl())
-        ]
-    )
+    # view = navigation_view(
+    #     page=page,
+    #     contents=[
+    #         NavigationContent(title="Item 1", icon=ft.icons.ADD_TO_HOME_SCREEN_SHARP, view=map()),
+    #         NavigationContent(title="Item 2", icon=ft.icons.ADD_COMMENT, view=ft.Text("Item 2")),
+    #         NavigationContent(title="Item 3", icon=ft.icons.ADD_COMMENT, view=SampleControl())
+    #     ]
+    # )
     
-    await page.add_async(view)
+    # await page.add_async(view)
+    
+    button1 = ft.CupertinoFilledButton("sample1", padding=2)
+    button2 = ft.CupertinoFilledButton("sample2", padding=2)
+    button3 = ft.CupertinoFilledButton("sample3", padding=2)
+    
+    col = ft.Column([
+        ft.Text("sample"),
+        ft.Container(content=
+            ft.Column(
+                [
+                    ft.Row([
+                        ft.Row([
+                            ft.Text("aaaaaaaa", expand=1),
+                            ft.Text("lasdjflsa", expand=1),
+                        ], expand=2),
+                        ft.Row([
+                            button1,
+                            button2,
+                            button3,
+                        ], expand=3, alignment=ft.MainAxisAlignment.CENTER)
+                    ]),
+                    ft.Row([
+                        ft.Row([
+                            ft.Text("aaaaaaaa", expand=1),
+                            ft.Text("lasdjflsa", expand=1),
+                        ], expand=2),
+                        ft.Row([
+                            button1,
+                            button2,
+                            button3,
+                        ], expand=3, alignment=ft.MainAxisAlignment.CENTER)
+                    ]),
+                    ft.Row([
+                        ft.Row([
+                            ft.Text("aaaaaaaa", expand=1),
+                            ft.Text("lasdjflsa", expand=1),
+                        ], expand=2),
+                        ft.Row([
+                            button1,
+                            button2,
+                            button3,
+                        ], expand=3, alignment=ft.MainAxisAlignment.CENTER)
+                    ]),
+                    ft.Row([
+                        ft.Row([
+                            ft.Text("aaaaaaaa", expand=1),
+                            ft.Text("lasdjflsa", expand=1),
+                        ], expand=2),
+                        ft.Row([
+                            button1,
+                            button2,
+                            button3,
+                        ], expand=3, alignment=ft.MainAxisAlignment.CENTER)
+                    ]),
+                    ft.Row([
+                        ft.Row([
+                            ft.Text("aaaaaaaa", expand=1),
+                            ft.Text("lasdjflsa", expand=1),
+                        ], expand=2),
+                        ft.Row([
+                            button1,
+                            button2,
+                            button3,
+                        ], expand=3, alignment=ft.MainAxisAlignment.CENTER)
+                    ]),
+                ],
+                expand=True,
+                alignment=ft.MainAxisAlignment.CENTER,
+                scroll=ft.ScrollMode.ALWAYS
+            ),
+            bgcolor=ft.colors.AMBER,
+            expand=1,
+            padding=10,),
+        ft.Container(content=ft.Row([]), bgcolor=ft.colors.INDIGO, expand=4),
+    ], expand=True)
 
+
+    page.add(col)
 
 
 if __name__ == "__main__":
